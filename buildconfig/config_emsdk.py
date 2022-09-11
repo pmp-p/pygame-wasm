13ee3b4ab58eaee12372a112f650a2bdd252559c
pmp-p <mail.peny@free.fr>
2022-09-11 11:18:05 +0200

    add https://github.com/pygame/pygame/pull/3439

--- buildconfig/config_emsdk.py
+++ buildconfig/config_emsdk.py
@@ -7,31 +7,43 @@ from glob import glob
 
 from distutils.sysconfig import get_python_inc
 
-configcommand = os.environ.get('SDL_CONFIG', 'sdl-config',)
-configcommand = configcommand + ' --version --cflags --libs'
-localbase = os.environ.get('LOCALBASE', '')
-if os.environ.get('PYGAME_EXTRA_BASE'):
-    extrabases = os.environ['PYGAME_EXTRA_BASE'].split(':')
+configcommand = os.environ.get(
+    "SDL_CONFIG",
+    "sdl-config",
+)
+configcommand = configcommand + " --version --cflags --libs"
+localbase = os.environ.get("LOCALBASE", "")
+if os.environ.get("PYGAME_EXTRA_BASE"):
+    extrabases = os.environ["PYGAME_EXTRA_BASE"].split(":")
 else:
     extrabases = []
 
-EMSDK = os.environ.get('EMSDK', None)
-is_wasm = EMSDK is not None
-if not is_wasm:
-    print("EMSDK not found")
-    raise SystemExit(1)
 
-# EMCC_CFLAGS="-s USE_SDL=2 is required to prevent '-iwithsysroot/include/SDL'
-# which is SDL1 from ./emscripten/tools/ports/__init__.py
-# but CI is not expected to have that problem and that would trigger useless
-# rebuild
-if os.environ.get("CI", None):
-    EMCC_CFLAGS = os.environ.get("EMCC_CFLAGS","")
+EMCC_CFLAGS = os.environ.get("EMCC_CFLAGS", "")
+
+# user build, make sure ports are pulled in.
+if os.environ.get("SDK_VERSION", None) is None:
     EMCC_CFLAGS += " -sUSE_SDL=2 -sUSE_LIBPNG -sUSE_LIBJPEG"
-    os.environ["EMCC_CFLAGS"]=EMCC_CFLAGS.strip()
+else:
+    # make sure CI only pick SDK components.
+    SDKROOT = os.environ.get("SDKROOT", "/opt/python-wasm-sdk")
+    emcc_cflags = [
+        # only static build is supported at the moment
+        "-DBUILD_STATIC",
+        # for SDL2_image
+        f"-I{SDKROOT}/devices/emsdk/usr/include/SDL2",
+        # ftbuild.h
+        f"-I{SDKROOT}/emsdk/upstream/emscripten/cache/sysroot/include/freetype2",
+        # avoid some cython generated warnings.
+        f"-Wno-unreachable-code-fallthrough {EMCC_CFLAGS}",
+    ]
+    EMCC_CFLAGS = " ".join(emcc_cflags)
+os.environ["EMCC_CFLAGS"] = EMCC_CFLAGS.strip()
 
-CC=os.environ.get("CC","emcc")
+
+CC = os.environ.get("CC", "emcc")
 os.environ["CC"] = CC.strip()
+
 
 
 class DependencyProg:
@@ -98,7 +110,7 @@ class Dependency:
         self.found = 0
         self.checklib = checklib
         self.checkhead = checkhead
-        self.cflags = ''
+        self.cflags = ""
 
     def configure(self, incdirs, libdirs):
         incname = self.checkhead
@@ -113,31 +125,37 @@ class Dependency:
         for dir in libdirs:
             for name in libnames:
                 path = os.path.join(dir, name)
-                if any(map(os.path.isfile, glob(path+'*'))):
+                if any(map(os.path.isfile, glob(path + "*"))):
                     self.lib_dir = dir
 
-        if (incname and self.lib_dir and self.inc_dir) or (not incname and self.lib_dir):
-            print(self.name + '        '[len(self.name):] + ': found')
+        if (incname and self.lib_dir and self.inc_dir) or (
+            not incname and self.lib_dir
+        ):
+            print(self.name + "        "[len(self.name) :] + ": found")
             self.found = 1
         else:
 
-            if self.name in ["FONT","IMAGE","MIXER","PNG","JPEG","FREETYPE"]:
+            if self.name in ["FONT", "IMAGE", "MIXER", "PNG", "JPEG", "FREETYPE"]:
                 self.found = 1
-                print(self.name + '        '[len(self.name):] + ': FORCED (via emsdk builtins)')
+                print(
+                    self.name
+                    + "        "[len(self.name) :]
+                    + ": FORCED (via emsdk builtins)"
+                )
                 return
-            print(self.name + '        '[len(self.name):] + ': not found')
+            print(self.name + "        "[len(self.name) :] + ": not found")
             print(self.name, self.checkhead, self.checklib, incdirs, libdirs)
 
 
 class DependencyPython:
     def __init__(self, name, module, header):
         self.name = name
-        self.lib_dir = ''
-        self.inc_dir = ''
+        self.lib_dir = ""
+        self.inc_dir = ""
         self.libs = []
-        self.cflags = ''
+        self.cflags = ""
         self.found = 0
-        self.ver = '0'
+        self.ver = "0"
         self.module = module
         self.header = header
 
@@ -155,38 +173,44 @@ class DependencyPython:
             else:
                 self.inc_dir = os.path.split(fullpath)[0]
         if self.found:
-            print(self.name + '        '[len(self.name):] + ': found', self.ver)
+            print(self.name + "        "[len(self.name) :] + ": found", self.ver)
         else:
-            print(self.name + '        '[len(self.name):] + ': not found')
+            print(self.name + "        "[len(self.name) :] + ": not found")
 
-sdl_lib_name = 'SDL'
+
+sdl_lib_name = "SDL"
+
 
 def main(auto_config=False):
     global origincdirs, origlibdirs
 
-    #these get prefixes with $EMSDK
-    origincdirs = ['/include']
+    # these get prefixes with $EMSDK
+    origincdirs = ["/include"]
     origlibdirs = []
 
-    print('\nHunting dependencies...')
+    print("\nHunting dependencies...")
 
     DEPS = [
-        DependencyProg('SDL', 'SDL_CONFIG', 'sdl2-config', '2.0', ['sdl']),
-        Dependency('FONT', 'SDL_ttf.h', 'libSDL2_ttf.so', ['SDL2_ttf']),
-        Dependency('IMAGE', 'SDL_image.h', 'libSDL2_image.a', ['SDL2_image']),
-        Dependency('MIXER', 'SDL_mixer.h', 'libSDL2_mixer.a', ['SDL2_mixer']),
-        Dependency('FREETYPE', 'ft2build.h', 'libfreetype.a', []),
-        #Dependency('GFX', 'SDL_gfxPrimitives.h', 'libSDL2_gfx.a', ['SDL2_gfx']),
+        DependencyProg("SDL", "SDL_CONFIG", "sdl2-config", "2.0", ["sdl"]),
+        Dependency("FONT", "SDL_ttf.h", "libSDL2_ttf.so", ["SDL2_ttf"]),
+        Dependency("IMAGE", "SDL_image.h", "libSDL2_image.a", ["SDL2_image"]),
+        Dependency("MIXER", "SDL_mixer.h", "libSDL2_mixer.a", ["SDL2_mixer"]),
+        Dependency("FREETYPE", "ft2build.h", "libfreetype.a", []),
+        # Dependency('GFX', 'SDL_gfxPrimitives.h', 'libSDL2_gfx.a', ['SDL2_gfx']),
     ]
-    DEPS.extend([
-        Dependency('PNG', 'png.h', 'libpng', ['png']),
-        Dependency('JPEG', 'jpeglib.h', 'libjpeg', ['jpeg']),
-        #Dependency('SCRAP', '', 'libX11', ['X11']),
-        #Dependency('GFX', 'SDL_gfxPrimitives.h', 'libSDL_gfx.a', ['SDL_gfx']),
-    ])
+    DEPS.extend(
+        [
+            Dependency("PNG", "png.h", "libpng", ["png"]),
+            Dependency("JPEG", "jpeglib.h", "libjpeg", ["jpeg"]),
+            # Dependency('SCRAP', '', 'libX11', ['X11']),
+            # Dependency('GFX', 'SDL_gfxPrimitives.h', 'libSDL_gfx.a', ['SDL_gfx']),
+        ]
+    )
 
     if not DEPS[0].found:
-        raise RuntimeError('Unable to run "sdl-config". Please make sure a development version of SDL is installed.')
+        raise RuntimeError(
+            'Unable to run "sdl-config". Please make sure a development version of SDL is installed.'
+        )
 
     incdirs = []
     libdirs = []
@@ -194,20 +218,20 @@ def main(auto_config=False):
     incdirs += [os.environ.get("PREFIX", "") + d for d in origincdirs]
     libdirs += [os.environ.get("PREFIX", "") + d for d in origlibdirs]
 
-    incdirs += [EMSDK + '/upstream/emscripten/cache/sysroot'+ d for d in origincdirs]
+    incdirs += [EMSDK + "/upstream/emscripten/cache/sysroot" + d for d in origincdirs]
 
     for extrabase in extrabases:
         incdirs += [extrabase + d for d in origincdirs]
         libdirs += [extrabase + d for d in origlibdirs]
 
     if localbase:
-        incdirs = [localbase+d for d in origincdirs]
-        libdirs = [localbase+d for d in origlibdirs]
+        incdirs = [localbase + d for d in origincdirs]
+        libdirs = [localbase + d for d in origlibdirs]
 
     for arg in DEPS[0].cflags.split():
-        if arg[:2] == '-I':
+        if arg[:2] == "-I":
             incdirs.append(arg[2:])
-        elif arg[:2] == '-L':
+        elif arg[:2] == "-L":
             libdirs.append(arg[2:])
     for d in DEPS:
         d.configure(incdirs, libdirs)
@@ -226,6 +250,8 @@ def main(auto_config=False):
     return DEPS
 
 
-if __name__ == '__main__':
-    print("""This is the configuration subscript for Unix.
-Please run "config.py" for full configuration.""")
+if __name__ == "__main__":
+    print(
+        """This is the configuration subscript for Unix.
+Please run "config.py" for full configuration."""
+    )
