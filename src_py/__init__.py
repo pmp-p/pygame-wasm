aaf812b2c0230d57753912ea409b21ca4cd71093
pmp-p <mail.peny@free.fr>
2022-12-30 07:34:55 +0100

    fix startup

--- src_py/__init__.py
+++ src_py/__init__.py
@@ -83,6 +83,37 @@ class MissingModule:
         except ImportError:
             print(message)
 
+# this is a special loader for WebAssembly platform
+# where pygame is actually statically linked
+# mixing single phase (C) and multiphase modules (cython)
+if sys.platform in ("wasi", "emscripten"):
+    try:
+        import pygame_static
+    except ModuleNotFoundError:
+        pygame_static = None
+
+    if pygame_static:
+
+        pygame = sys.modules[__name__]
+
+        pygame.Color = pygame.color.Color
+
+        Vector2 = pygame.math.Vector2
+        Vector3 = pygame.math.Vector3
+
+        Rect = pygame.rect.Rect
+
+        BufferProxy = pygame.bufferproxy.BufferProxy
+
+        # cython modules use multiphase initialisation when not in builtin Inittab.
+
+        from pygame import _sdl2
+
+        import importlib.machinery
+
+        loader = importlib.machinery.FrozenImporter
+        spec = importlib.machinery.ModuleSpec("", loader)
+        pygame_static.import_cython(spec)
 
 # we need to import like this, each at a time. the cleanest way to import
 # our modules is with the import command (not the __import__ function)
@@ -331,6 +362,12 @@ def __color_reduce(c):
 
 
 copyreg.pickle(Color, __color_reduce, __color_constructor)
+
+if sys.platform in ("wasi", "emscripten"):
+    if pygame_static:
+        import pygame.wasm_patches
+
+        print(sys._emscripten_info)
 
 # Thanks for supporting pygame. Without support now, there won't be pygame later.
 if "PYGAME_HIDE_SUPPORT_PROMPT" not in os.environ:
