7950fa4dd8c64f1e1882b821fc13f97d04fc396f
pmp-p <mail.peny@free.fr>
2022-12-30 08:28:32 +0100

    fix _sdl2 import on wasm

--- src_py/_sdl2/__init__.py
+++ src_py/_sdl2/__init__.py
@@ -1,3 +1,4 @@
+import sys
 if not sys.platform in ("wasi", "emscripten"):
     from .sdl2 import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
     from .audio import *  # pylint: disable=wildcard-import; lgtm[py/polluting-import]
