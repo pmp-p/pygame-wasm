27fa819be2ac7416688bab4dea7f6d635c11c576
pmp-p <mail.peny@free.fr>
2022-12-30 06:37:39 +0100

    fix set_mode

--- src_py/wasm_patches.py
+++ src_py/wasm_patches.py
@@ -2,6 +2,8 @@ import sys
 import asyncio
 import json
 
+from platform import window, is_browser
+
 from pathlib import Path
 
 #=================================================
@@ -12,7 +14,6 @@ import aio.gthread
 from threading import Thread
 #=================================================
 
-from platform import window
 
 
 pygame = sys.modules[__package__]
@@ -71,24 +72,25 @@ pygame.quit = patch_pygame_quit
 __pygame_display_set_mode__ = pygame.display.set_mode
 
 
-def patch_pygame_display_set_mode(size=(0,0), flags=0, depth=0):
-    import platform
+def patch_pygame_display_set_mode(size=(0,0), flags=0, depth=0, display=0, vsync=0):
 
     # apparently no need to remove scaled.
     if size != (0,0):
-        if (sys.platform == "emscripten") and platform.is_browser:
+        if (sys.platform == "emscripten") and is_browser:
             try:
-                platform.window.window_resize()
+                window.window_resize()
             except:
-                print("ERROR: browser host does not provide window_resize() function",file=sys.__stderr__)
+                print("ERROR: browser host does not provide window_resize() function", file=sys.__stderr__)
 
-    return __pygame_display_set_mode__(size, flags, depth)
+    return __pygame_display_set_mode__(size, flags, depth, display, vsync)
 
 
 pygame.display.set_mode = patch_pygame_display_set_mode
 
 
 #=======================================================================
+# pygame.mixer.music
+#
 # replace sdl thread music playing by browser native player
 #
 
@@ -107,16 +109,14 @@ def patch_pygame_mixer_music_load(fileobj, namehint=""):
 
     global tracks
 
+    # stop previously loaded track
     patch_pygame_mixer_music_stop_pause_unload()
 
     tid = tracks.get( fileobj, None)
 
-    # stop previously loaded track
-    if tid is not None:
-        window.MM.stop(tid)
-    else:
-        # track was never loaded before
-        track = patch_pygame_mixer_sound(fileobj, auto=True)
+    # track was never loaded before
+    if tid is None:
+        track = patch_pygame_mixer_sound(fileobj, auto=False)
         tid = track.trackid
 
     # set new current track
@@ -160,37 +160,87 @@ def patch_pygame_mixer_sound(data, auto=False):
     window.MM.load(track.trackid)
     return track
 
-def patch_pygame_mixer_SoundPatch():
-    print("pygame mixer SFX patch is now active")
-    pygame.mixer.Sound = patch_pygame_mixer_sound
+BUFFERSIZE = 2048
 
-pygame.mixer.SoundPatch = patch_pygame_mixer_SoundPatch
 
+def patch_pygame_mixer_music_set_volume(vol:float):
+    if vol<0:return
+    if vol>1:vol=1.0
+    trackid = tracks["current"]
+    if trackid:
+        window.MM.set_volume(trackid, vol)
+    else:
+        pdb(__file__, "ERROR 175: no track is loaded")
+pygame.mixer.music.set_volume = patch_pygame_mixer_music_set_volume
+
+
+def patch_pygame_mixer_music_get_volume():
+    trackid = tracks["current"]
+    return float(window.MM.get_volume(trackid))
+pygame.mixer.music.get_volume = patch_pygame_mixer_music_get_volume
 
 def patch_pygame_mixer_music_play(loops=0, start=0.0, fade_ms=0):
     trackid = tracks["current"]
     if trackid:
-        window.MM.stop(trackid)
+        window.MM.pause(trackid)
         window.MM.play(trackid, loops )
     else:
-        pdb(__file__, "ERROR 156: no track is loaded")
+        pdb(__file__, "ERROR 184: no track is loaded")
 
 pygame.mixer.music.play = patch_pygame_mixer_music_play
 
+def patch_pygame_mixer_music_pause():
+    last = tracks["current"]
+    if last:
+        window.MM.pause(last)
+
+
+def patch_pygame_mixer_music_stop():
+    last = tracks["current"]
+    if last:
+        window.MM.stop(last)
+
+pygame.mixer.music.stop = patch_pygame_mixer_music_stop
+pygame.mixer.music.pause = patch_pygame_mixer_music_pause
+
+# TODO:
+# https://www.pygame.org/docs/ref/music.html#pygame.mixer.music.fadeout
+
+#=======================================================================
+# pygame.mixer.Sound
 
 
 
 
 
+# 0.2.0+ use huge buffer size instead of patching whole module.
+# pro: code compatibility
+# con: sound lag
+pygame.mixer.pre_init(buffer = BUFFERSIZE)
+
+if 0:
+    # 0.1.6 used to force soundpatch
+    def patch_pygame_mixer_SoundPatch():
+        print("pygame mixer SFX patch is already active you can remove this call")
+
+else:
+
+    def patch_pygame_mixer_SoundPatch():
+        pygame.mixer.Sound = patch_pygame_mixer_sound
+        print("pygame mixer SFX patch is now active")
 
 
+    __pygame_mixer_init = pygame.mixer.init
 
+    def patch_pygame_mixer_init(frequency=44100, size=-16, channels=2, buffer=512, devicename=None, allowedchanges=0) -> None:
+        global BUFFERSIZE
+        buffer = BUFFERSIZE
+        print(f"pygame mixer init {frequency=}, {size=}, {channels=}, {buffer=}" )
+        __pygame_mixer_init(frequency, size, channels, buffer)
 
+    pygame.mixer.init = patch_pygame_mixer_init
 
-
-
-
-
+pygame.mixer.SoundPatch = patch_pygame_mixer_SoundPatch
 
 
 
